9c1ad07bf61021d0f9cdb9dfab57bc0296624172
mbaireddy <136340930+mbaireddy@users.noreply.github.com>
2025-02-01 18:24:09 -0500

    Create Problem1.java

---
+++ PracticeSet2024/Problem1.java
@@ -0,0 +1,13 @@
+package PracticeSet2024;
+/* 
+* https://apcentral.collegeboard.org/courses/ap-computer-science-a/exam/past-exam-questions
+* 2024
+*/
+
+public class Problem1{
+
+    public static void main(String[] arg){
+        System.out.println("hello");
+
+    }
+}
